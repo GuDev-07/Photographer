261b8f5a6af345844c1beedd4e69cebf4468f068
Gustavo <gustavinhojesussant@gmail.com>
2024-11-07 00:00:01 -0300

    vinculando ao meu e-mail e atualizando

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ navLinks.forEach(link =>{
         navbarLinks.classList.remove('active'); //Fecha o menu
     });
 });
+
+//Redirecionamento do E-mail
+function sendEmail() {
+  console.log("sendEmail function called");
+
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value; 
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  console.log("Captured Data:", { name, phone, email, message });
+
+  emailjs.send("service_wdt3hil", "template_tvu0xjy", {
+      name: name,
+      phone: phone,
+      email: email,
+      message: message,
+  })
+  .then(function(response) {
+      console.log("Email enviado com sucesso", response.status, response.text);
+      window.location.href = 'obrigado.html';
+  }, function(error) {
+      console.error("Erro ao enviar e-mail:", error);
+      alert("Falha ao enviar a mensagem: " + JSON.stringify(error));
+  });
+}
